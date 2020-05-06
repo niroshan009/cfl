b36e5cc1b6de1b757f2745bf5fc421e781f1a92f
thusitha90 <dudeniro@gmail.com>
2020-05-06 14:28:30 +0800

    get page count

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,15 +12,15 @@ export class DataService {
 
   getPlayerPageCount(): Observable<number> {
     let api = '';
-    let pageCount: number;
+
     return this.http
       .get<any>(api)
-      .pipe(map((data) => (pageCount = Math.ceil(data.data.length / 10))));
+      .pipe(map((data) => Math.ceil(data.data.length / 10)));
   }
 
   getPlayers(pageNumber: number): Observable<IPlayer[]> {
     let api: string = '';
-    let players: IPlayer[];
+
     return this.http.get<any>(api).pipe(map((data) => (players = data.data)));
   }
 }
